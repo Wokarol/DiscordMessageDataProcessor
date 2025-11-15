c0ee42660c038c2a22d19970c509ae9778cfd0a7
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2025-11-15 02:11:09 +0100

    Init

---
+++ Processor.cs
@@ -0,0 +1,134 @@
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+namespace DiscordMessageDataProcessor;
+
+public partial class Processor
+{
+    public Dictionary<string, List<ChannelData>> ChannelsByServerNames { get; } = [];
+    public List<DirectMessagesData> DirectMessages { get; } = [];
+    public Dictionary<string, ExtraChannelMetadata> ExtraChannelMetadataById { get; } = [];
+
+    public void LoadData(string folderPath)
+    {
+        string messagesFolderPath = Path.Combine(folderPath, "Messages");
+        string channelsIndexPath = Path.Combine(messagesFolderPath, "index.json");
+
+        if (!Directory.Exists(folderPath))
+            throw new DirectoryNotFoundException(folderPath);
+
+        if (!Directory.Exists(messagesFolderPath))
+            throw new DirectoryNotFoundException(messagesFolderPath);
+
+        if (!File.Exists(channelsIndexPath))
+            throw new FileNotFoundException(channelsIndexPath);
+
+        using var indexFile = File.OpenRead(channelsIndexPath);
+
+        var channelIndexRaw = JsonSerializer.Deserialize<Dictionary<string, string>>(indexFile) ?? throw new Exception("Failed to load the channel index");
+
+        ChannelsByServerNames.Clear();
+        DirectMessages.Clear();
+
+        foreach (var (id, fullChannelName) in channelIndexRaw)
+        {
+            if (ChannelRegex().IsMatch(fullChannelName))
+            {
+                // The channel is a channel in the server
+                var match = ChannelRegex().Match(fullChannelName);
+                var channelName = match.Groups["Name"].Value;
+                var channelServer = match.Groups["Server"].Value;
+
+                if (channelName == "Unknown channel")
+                    continue;
+
+                if (!ChannelsByServerNames.TryGetValue(channelServer, out var channels))
+                {
+                    channels = new List<ChannelData>();
+                    ChannelsByServerNames[channelServer] = channels;
+                }
+
+                channels.Add(new(channelName, id));
+            }
+            else if (DirectMessageRegex().IsMatch(fullChannelName))
+            {
+                // The channel is a direct message
+                var match = DirectMessageRegex().Match(fullChannelName);
+                var username = match.Groups["User"].Value;
+
+                DirectMessages.Add(new(fullChannelName, username, id));
+            }
+            else if(fullChannelName == "Direct Message with Unknown Participant" || fullChannelName == "None")
+            {
+                // We don't care about that
+            }
+            else
+            {
+                // A group chat... probably
+                DirectMessages.Add(new(fullChannelName, null, id));
+            }
+        }
+    }
+
+    public void LoadChannelMetadata(string folderPath, List<ChannelData> channelsToLoad)
+    {
+        string messagesFolderPath = Path.Combine(folderPath, "Messages");
+
+        if (!Directory.Exists(folderPath))
+            throw new DirectoryNotFoundException(folderPath);
+
+        if (!Directory.Exists(messagesFolderPath))
+            throw new DirectoryNotFoundException(messagesFolderPath);
+
+        foreach (var channelData in channelsToLoad)
+        {
+            string channelFolderPath = Path.Combine(messagesFolderPath, $"c{channelData.Id}");
+            string channelDataPath = Path.Combine(channelFolderPath, $"channel.json");
+
+            if (!Directory.Exists(channelFolderPath))
+                throw new DirectoryNotFoundException(channelFolderPath);
+
+            if (!File.Exists(channelDataPath))
+                throw new FileNotFoundException(channelDataPath);
+
+            using var channelDataFile = File.OpenRead(channelDataPath);
+
+            var extraChannelDataRaw = JsonSerializer.Deserialize<ExtraChannelMetadataRaw>(channelDataFile) ?? throw new Exception("Failed to load the channel index");
+
+            var type = extraChannelDataRaw.type switch
+            {
+                "PRIVATE_THREAD" => ChannelType.PrivateThread,
+                "PUBLIC_THREAD" => ChannelType.PublicThread,
+                "GUILD_TEXT" => ChannelType.Text,
+                "GUILD_VOICE" => ChannelType.Voice,
+                _ => ChannelType.Invalid,
+            };
+
+            ExtraChannelMetadataById[channelData.Id] = new ExtraChannelMetadata(channelData.Id, type);
+        }
+    }
+
+    public record ChannelData(string Name, string Id);
+
+    /// <param name="Username">If null, it's a group channel</param>
+    public record DirectMessagesData(string Name, string? Username, string Id);
+    public record ExtraChannelMetadataRaw(string id, string type);
+    public record ExtraChannelMetadata(string Id, ChannelType Type);
+
+    public enum ChannelType
+    {
+        Invalid,
+        PrivateThread,
+        PublicThread,
+        Text,
+        Voice,
+    }
+
+
+    [GeneratedRegex(@"^(?<Name>.+)\sin\s(?<Server>.+)$")]
+    private static partial Regex ChannelRegex();
+
+    [GeneratedRegex(@"^Direct Message with (?<User>.+?)#\d+$")]
+    private static partial Regex DirectMessageRegex();
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿global using Spectre.Console;
+
+using DiscordMessageDataProcessor;
+
+
+string folderPath = "discord-package";
+
+string fullFolderPath = Path.GetFullPath(folderPath);
+
+AnsiConsole.MarkupLineInterpolated($"Loading file at [blue]{fullFolderPath}[/]");
+
+var processor = new Processor();
+
+// Loading the data
+{
+    try
+    {
+        processor.LoadData(fullFolderPath);
+    }
+    catch (DirectoryNotFoundException e)
+    {
+        AnsiConsole.MarkupLineInterpolated($"[red]Cannot find directory: {e.Message}[/]");
+    }
+    catch (FileNotFoundException e)
+    {
+        AnsiConsole.MarkupLineInterpolated($"[red]Cannot find file: {e.Message}[/]");
+    }
+    catch (Exception e)
+    {
+        AnsiConsole.MarkupLineInterpolated($"[red]Failed to load the data: {e.Message}[/]");
+    }
+
+
+    AnsiConsole.MarkupLineInterpolated($"Loaded the index");
+    AnsiConsole.MarkupLineInterpolated($"    [teal]{"Servers",-20}[/] {processor.ChannelsByServerNames.Count}");
+    AnsiConsole.MarkupLineInterpolated($"    [teal]{"Direct Messages",-20}[/] {processor.DirectMessages.Count}");
+}
+
+
+// Selecting the server and channels
+{
+    var selectedServer = AnsiConsole.Prompt(new SelectionPrompt<string>()
+        .Title("Select a server to process")
+        .PageSize(30)
+        .AddChoices(processor.ChannelsByServerNames.Keys.OrderBy(x => x))
+    );
+
+    var serverChannels = processor.ChannelsByServerNames[selectedServer] ?? throw new Exception();
+    processor.LoadChannelMetadata(folderPath, serverChannels);
+
+    var channelsGroupedByType = serverChannels
+        .Select(c => (Channel: c, Metadata: processor.ExtraChannelMetadataById[c.Id]))
+        .GroupBy(x => x.Metadata.Type);
+
+    AnsiConsole.MarkupLineInterpolated($"Loaded the server");
+    AnsiConsole.MarkupLineInterpolated($"    [teal]{"Channels (Total)",-20}[/] {serverChannels.Count}");
+    CountChannelsOfType(channelsGroupedByType, Processor.ChannelType.Text, "Text Channels");
+    CountChannelsOfType(channelsGroupedByType, Processor.ChannelType.Voice, "Voice Channels");
+    CountChannelsOfType(channelsGroupedByType, Processor.ChannelType.PrivateThread, "Private Threads");
+    CountChannelsOfType(channelsGroupedByType, Processor.ChannelType.PublicThread, "Public Threads");
+
+    var multiSelectionOfChannels = new MultiSelectionPrompt<Processor.ChannelData>()
+        .Title("Select channels to analyze")
+        .PageSize(30)
+        .UseConverter(c => Markup.Escape(c.Name));
+
+    AddChoicesForChannelType(multiSelectionOfChannels, channelsGroupedByType, Processor.ChannelType.Text, "Text Channels");
+    AddChoicesForChannelType(multiSelectionOfChannels, channelsGroupedByType, Processor.ChannelType.Voice, "Voice Channels");
+    AddChoicesForChannelType(multiSelectionOfChannels, channelsGroupedByType, Processor.ChannelType.PrivateThread, "Private Threads");
+    AddChoicesForChannelType(multiSelectionOfChannels, channelsGroupedByType, Processor.ChannelType.PublicThread, "Public Threads");
+
+    var selectedChannels = AnsiConsole.Prompt(multiSelectionOfChannels);
+
+
+    static void AddChoicesForChannelType(MultiSelectionPrompt<Processor.ChannelData> multiSelectionOfChannels, IEnumerable<IGrouping<Processor.ChannelType, (Processor.ChannelData Channel, Processor.ExtraChannelMetadata Metadata)>> channelsGroupedByType, Processor.ChannelType typeToAdd, string labelToAdd)
+    {
+        var group = channelsGroupedByType.FirstOrDefault(g => g.Key == typeToAdd);
+        if (group == null) return;
+
+        multiSelectionOfChannels.AddChoiceGroup(new Processor.ChannelData(labelToAdd, ""), group.Select(x => x.Channel).OrderBy(c => c.Name));
+    }
+
+    static void CountChannelsOfType(IEnumerable<IGrouping<Processor.ChannelType, (Processor.ChannelData Channel, Processor.ExtraChannelMetadata Metadata)>> channelsGroupedByType, Processor.ChannelType typeToList, string labelToShow)
+    {
+        var group = channelsGroupedByType.FirstOrDefault(g => g.Key == typeToList);
+        if (group == null) return;
+
+        AnsiConsole.MarkupLineInterpolated($"        [teal]{labelToShow,-20}[/] {group.Count()}");
+    }
+}
+
+
